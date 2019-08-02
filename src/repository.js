9a70ab3cb25aac97e393eeaa91a6921f8319bbd7
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2019-08-02 16:43:22 +0100

    Break down repository component

--- src/repository.js
+++ src/repository.js
@@ -1,38 +1,47 @@
 import React, { Fragment } from "react";
 
+const RepositoryHeader = ({ repository }) => (
+  <p>
+    <strong>In Repository:</strong>
+    <a href={repository.url}>{repository.name}</a>
+  </p>
+);
+
+const StarButton = ({ repository, onStarRepository }) => (
+  <button
+    type="button"
+    onClick={() => onStarRepository(repository.id, repository.viewerHasStarred)}
+  >
+    {repository.stargazers.totalCount}
+    {repository.viewerHasStarred ? " Unstar" : " Star"}
+  </button>
+);
+
+const IssueBulletPoint = ({ node }) => (
+  <li key={node.id}>
+    <a href={node.url}>{node.title}</a>
+
+    <ul>
+      {node.reactions.edges.map(reaction => (
+        <li key={reaction.node.id}>{reaction.node.content}</li>
+      ))}
+    </ul>
+  </li>
+);
+
 const Repository = ({ repository, onFetchMoreIssues, onStarRepository }) => (
   <Fragment>
-    <p>
-      <strong>In Repository:</strong>
-      <a href={repository.url}>{repository.name}</a>
-    </p>
+    <RepositoryHeader repository={repository} />
 
-    <button
-      type="button"
-      onClick={() =>
-        onStarRepository(repository.id, repository.viewerHasStarred)
-      }
-    >
-      {repository.stargazers.totalCount}
-      {repository.viewerHasStarred ? " Unstar" : " Star"}
-    </button>
+    <StarButton repository={repository} onStarRepository={onStarRepository} />
 
     <ul>
       {repository.issues.edges.map(issue => (
-        <li key={issue.node.id}>
-          <a href={issue.node.url}>{issue.node.title}</a>
-
-          <ul>
-            {issue.node.reactions.edges.map(reaction => (
-              <li key={reaction.node.id}>{reaction.node.content}</li>
-            ))}
-          </ul>
-        </li>
+        <IssueBulletPoint node={issue.node} />
       ))}
     </ul>
 
     <hr />
-
     {repository.issues.pageInfo.hasNextPage && (
       <button onClick={onFetchMoreIssues}>More</button>
     )}
